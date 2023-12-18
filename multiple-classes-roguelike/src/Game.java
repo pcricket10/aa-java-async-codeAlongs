408544f3392e25faf0ec899823f897acb793a0c4
Philip McCrickard <pcricket10@gmail.com>
2023-12-18 17:01:27 -0500

    finished

--- multiple-classes-roguelike/src/Game.java
+++ multiple-classes-roguelike/src/Game.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 public class Game {
     // constants
     private final static int WIDTH = 10;
-    private final static String WALL_CHARACTER = "#";
+    private final static String WALL_CHARACTER = "M";
     private final static String EMPTY_CHARACTER = " ";
 
     private final Scanner console = new Scanner(System.in);
     private Hero hero;
+    private Treasure treasure;
     private boolean isOver;
 
     public void run() {
@@ -17,6 +18,7 @@ public class Game {
             printWorld();
             move();
         }
+        printWorld();
     }
     private void setUp() {
         System.out.print("What is the name of your hero?: ");
@@ -27,6 +29,13 @@ public class Game {
         int y = rand.nextInt(WIDTH);
 
         hero = new Hero(name, x, y);
+
+        do {
+            x = rand.nextInt(WIDTH);
+            y = rand.nextInt(WIDTH);
+        } while (x == hero.getX() && y == hero.getY());
+
+        treasure = new Treasure(x, y);
     }
     private void printWorld(){
         // top wall border
@@ -36,12 +45,13 @@ public class Game {
             // left wall border
             System.out.print(WALL_CHARACTER);
             for (int col = 0; col < WIDTH; col++) {
-                if(row == hero.getY() && col == hero.getX()){
+                if (row == hero.getY() && col == hero.getX()) {
                     System.out.print(hero.getSymbol());
+                } else if (row == treasure.getY() && col == treasure.getX()) {
+                    System.out.print("T");
                 } else {
                     System.out.print(EMPTY_CHARACTER);
                 }
-
             }
             // right wall border
             System.out.println(WALL_CHARACTER);
@@ -58,28 +68,24 @@ public class Game {
         }
         switch (move.charAt(0)){
             case 'W':
-                if(hero.getY() - 1 >= 0) {
                     hero.moveUp();
-                }
                 break;
             case 'A':
-                if(hero.getX() - 1 >= 0) {
                     hero.moveLeft();
-                }
                 break;
             case 'S':
-                if(hero.getY() + 1 < WIDTH) {
                     hero.moveDown();
-                }
                 break;
             case 'D':
-                if(hero.getX() + 1 < WIDTH){
                     hero.moveRight();
-                }
                 break;
-            case'X':
-                isOver = true;
-                break;
+        }
+        if (hero.getX() < 0 || hero.getX() >= WIDTH || hero.getY() < 0 || hero.getY() >= WIDTH){
+            System.out.println(hero.getName() + " touched lava! You lose.");
+            isOver = true;
+        } else if (hero.getX() == treasure.getX() && hero.getY() == treasure.getY()){
+            System.out.println(hero.getName() + " found the treasure! You win.");
+            isOver = true;
         }
     }
 }
