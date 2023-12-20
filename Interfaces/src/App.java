7c6292f6967167097fc27f738ba539a918f18d3f
Philip McCrickard <pcricket10@gmail.com>
2023-12-20 16:32:50 -0500

    inheritance

---
+++ Interfaces/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        Vehicle truck = new Truck();
+        Vehicle spaceship = new Spaceship();
+        for (int i = 0; i < 7; i++) {
+            truck.accelerate();
+            spaceship.accelerate();
+
+        }
+
+        System.out.println("truck: " + truck.getVelocityMessage());
+        System.out.println("spaceship: " + spaceship.getVelocityMessage());
+    }
+}
